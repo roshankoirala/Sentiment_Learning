0a97b90fea472c96f1f0cee6b1d2cd019a4da6cd
Roshan Koirala <41840232+roshankoirala@users.noreply.github.com>
2020-08-29 15:27:54 -0400

    Add files via upload

---
+++ emoticons.py
@@ -0,0 +1,63 @@
+# https://github.com/NeelShah18/emot/blob/master/emot/emo_unicode.py
+# https://slangit.com/emoticons/common
+
+
+EMOTICONS = {':)': 'smile', 
+             ':-)': 'smile', 
+             ':^)': 'smile', 
+             ':-D': 'smile', 
+             ":'-)": 'sadsmile',
+             ';d': 'wink', 
+             ';)': 'wink', 
+             ';-)': 'wink',
+             ':(': 'sad', 
+             ':-(': 'sad', 
+             ':-<': 'sad', 
+             ':P': 'raspberry', 
+             ':O': 'surprised',
+             ':-@': 'shocked', 
+             ':@': 'shocked',
+             ':-$': 'confused',
+             ':-!': 'confused', 
+             ':-&': 'confused', 
+             'O.o': 'confused',
+             ':\\': 'annoyed', 
+             ':#': 'mute', 
+             ':X': 'mute', 
+             '$_$': 'greedy',
+             '@@': 'eyeroll', 
+             ':-0': 'yell', 
+             '<(-_-)>': 'robot', 
+             'd[-_-]b': 'dj', 
+             'O:-)': 'angel',
+             'O*-)': 'angel',
+             '(:-D': 'gossip',
+             '=^.^=': 'cat', 
+             ':-E': 'vampire', 
+#              'XD':
+#              '=D':
+#              ':|':
+#              ':-|':
+#              '~,~':
+#              '<3':
+#              ':-B':
+#              '<|:0':
+#              ':-/':
+#              ':/':
+#              '8-)':
+#              '>:)':
+#              ':*)':
+#              ':-#':
+#              '#-o':
+#              'B-)':
+#              ":')":
+#              '*-*':
+#              '<><':
+#              '</3':
+             
+        
+            }
+
+
+
+
